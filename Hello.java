00fe4fb28a3d78c87a82f45f023da7363454ee85
DW <924004+daw42@users.noreply.github.com>
2024-02-21 09:35:57 -0800

    Add TODO

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public static void foo() {
+        // TODO: Write this.
+    }
 }
